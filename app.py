662e4086dcf6e0f75469cc4dc8d9d18d4a37ad90
hadibagdadii <bagdadihadi@gmail.com>
2024-01-01 13:14:51 -0800

    posting form contents to route

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app.secret_key = 'ML_app'  # Set a secret key for session security
 def index():
     return render_template('webpage.html')
 
+@app.route('/get-features', methods=['POST'])
+def getFeatures():
+    price = request.form['Buying']
+    maint = request.form['maint']
+    doors = request.form['doors']
+    people = request.form['Persons']
+    trunk = request.form['lug_boot']
+    safety = request.form['safety']
+    print(price, maint, doors, people, trunk, safety)
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
