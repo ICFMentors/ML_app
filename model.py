302f6fe1baeb17a9bc98542f334a7074db9a8cf3
hadibagdadii <bagdadihadi@gmail.com>
2024-01-02 11:25:08 -0800

    model.py

---
+++ model.py
@@ -0,0 +1,35 @@
+# conventional way to import pandas
+import pandas as pd
+import numpy as np
+import pprint
+from numpy.testing import assert_almost_equal
+import onnx
+from onnx.tools.net_drawer import GetPydotGraph, GetOpNodeProducer
+import onnxruntime as rt
+import skl2onnx
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType, StringTensorType
+from skl2onnx.common.data_types import Int64TensorType
+car_eval_features = [['vhigh', 'vhigh', '2', '2', 'small', 'low']]
+input_df = pd.DataFrame(car_eval_features, columns=['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety'])
+inputs = {c: input_df[c].values for c in input_df.columns}
+for k in inputs:
+    inputs[k] = inputs[k].reshape((inputs[k].shape[0], 1))
+print(inputs)
+# setup runtime - load the persisted ONNX model
+sess = rt.InferenceSession("pipeline_car_eval_lr.onnx")
+outputs = sess.run(None,inputs)
+car_eval_class = outputs[0]
+#probabilities = outputs[1]
+#confidence_class = car_eval_class[0]
+print(car_eval_class)
+car_eval_features = [['med', 'low', '5more', '4', 'big', 'med']]
+input_df = pd.DataFrame(car_eval_features, columns=['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety'])
+inputs = {c: input_df[c].values for c in input_df.columns}
+for k in inputs:
+    inputs[k] = inputs[k].reshape((inputs[k].shape[0], 1))
+outputs = sess.run(None,inputs)
+car_eval_class = outputs[0]
+#probabilities = outputs[1]
+#confidence_class = car_eval_class[0]
+print(car_eval_class)
